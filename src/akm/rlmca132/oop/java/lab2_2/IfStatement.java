87ff0d32768b2dc522d64c577bacd10bbbe0a944
Anoopkumar Muttathil <anoopkumar.m@gmail.com>
2019-12-26 12:14:05 +0530

    Till Lab 2.2.1 Commit

---
+++ src/akm/rlmca132/oop/java/lab2_2/IfStatement.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package akm.rlmca132.oop.java.lab2_2;
+
+/**
+ *
+ * @author admin
+ */
+public class IfStatement {
+    public static void main ( String args [ ] )
+    {
+        int x = 4;
+        int y = 10;
+        if (x > y ){
+            System.out.println ("x is greater than y" );
+        }
+        else {
+            System.out.println ("X is lesser than y");
+        }
+    }
+}
