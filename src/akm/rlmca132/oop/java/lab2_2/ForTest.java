b1fc59473dcd1c7566bef44f2e00ddecad3d5b89
Anoopkumar Muttathil <anoopkumar.m@gmail.com>
2020-01-24 22:09:01 +0530

    Till Lab 2.2.2 Commit

---
+++ src/akm/rlmca132/oop/java/lab2_2/ForTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package akm.rlmca132.oop.java.lab2_2;
+
+/**
+ *
+ * @author admin
+ */
+class ForTest
+{
+    public static void main (String args [ ] )
+    {
+        int i= 0;
+        int sum = 0;
+        for( i = 0; i <= 10; i++)
+            sum += i;
+        System.out.println ("The sum of first 10 Nos =" + sum );
+    }
+}
---
+++ src/akm/rlmca132/oop/java/lab2_2/WhileTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package akm.rlmca132.oop.java.lab2_2;
+
+/**
+ *
+ * @author admin
+ */
+class WhileTest
+{
+    public static void main (String args [ ] )
+    {
+    int i=1;
+        while (i<=5)
+        {
+            System.out.println ("i =" + i);
+            i++;
+        }
+    }
+}
