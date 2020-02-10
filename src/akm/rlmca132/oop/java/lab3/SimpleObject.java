50b6db8808abcd0d7866f02fdcf32d50461f76e9
Anoopkumar Muttathil <anoopkumar.m@gmail.com>
2020-02-10 15:12:17 +0530

    Till Lab 2.2.2 Commit

---
+++ src/akm/rlmca132/oop/java/lab3/SimpleObject.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package akm.rlmca132.oop.java.lab3;
+
+/**
+ *
+ * @author admin
+ */
+class SimpleObject
+{
+    SimpleObject(){
+        System.out.println ("No argument Constructor");
+    }
+    SimpleObject(int a){
+        System.out.println ("One argument Constructor with " + a);
+    }
+}
