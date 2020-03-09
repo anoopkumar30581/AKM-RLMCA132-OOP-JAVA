3b59480513d4a775640fa77f636d51a78cd0062c
Anoopkumar Muttathil <anoopkumar.m@gmail.com>
2020-03-09 16:31:40 +0530

    Package lab Commit

---
+++ src/akm/rlmca132/oop/java/akmpackage/MyClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package akmpackage;
+// Name of the package must be same as the directory
+// under which this fi
+
+public class MyClass
+{
+    public void getNames(String s)
+    {        
+        System.out.println(s);        
+    }
+}
